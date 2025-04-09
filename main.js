55ef98f6bc26236486a03e2cf3a1fe0b275d32e5
Ramonpol10 <ramonzinevil369@gmai.com>
2025-04-08 21:50:01 -0300

    inclusão do java

---
+++ main.js
@@ -0,0 +1,50 @@
+function colorirDia(){
+    // Pegar valor de campo de dia
+    let days = document.getElementById('days').value;
+
+    // Pegar o valor do campo de cor
+    let color = document.getElementById('color').value;
+
+    // Pegar os elementos da tabela calendario
+    let calendar = document.getElementById('calendas');
+
+    // Verificar se o campo de dia foi preenchido
+    if(!days){
+        alert('Favor informar o dia');
+    } else if ((days > 0) && (days < 31)){
+        // Se o numero de dias esta no intervalor
+        //aplicará a cord na célula do calendário
+        let td = calendar.getElementsByTagName('td');
+        td.sytle.backgroundColor = color;
+    } else{
+        alert("Favor informar um dia do calendário");
+
+}
+
+// Guardar a célula da tebela
+let elementos = document.querySelectorAll('td');
+
+// Declara e inicializa contadores
+let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
+
+// Loop para verificar o estilo de cada célula e contar quantas tem cor e qual cor
+for (let i = 0; i < elementos.length; i++){
+ // obtém o estilo computado do elemento atual
+ var estilo = window.getComputedStyle(elementos [i])
+
+ // Obtém a cor de fundo atual de célula
+ var corEstilo = estilo.backgroundColor;
+
+ // Verificar a cor e incrementa contador correspondente
+ if(corEstilo === "rgb(173, 216, 230)" || corEstilo === "lightblue"){
+    contadorAzul++;
+ } else if (corEstilo === "rgb(152, 215,152)" || corEstilo === "palegreen"){
+    contadorVerde++;
+ } else if(corEstilo === "rgb(255, 182, 193)" || corEstilo === "lightpink"){
+    contadorRosa++;
+ } else if (corEstilo === "rgb(106, 90, 205)" || corEstilo === "slateblue"){
+    contadorRoxo++;
+ }
+}
+
+}
